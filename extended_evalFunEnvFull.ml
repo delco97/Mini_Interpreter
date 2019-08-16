fd8f66636cbe4e25959878b91d391b5201936e81
Andrea Del Corto <andrea.delcorto@gmail.com>
2019-08-16 19:28:17 +0200

    Select implemented

--- extended_evalFunEnvFull.ml
+++ extended_evalFunEnvFull.ml
@@ -3,18 +3,22 @@ type exp = Eint of int | Ebool of bool | Den of ide | Prod of exp * exp | Sum of
 	Minus of exp | IsZero of exp | Or of exp * exp | And of exp * exp | Not of exp | Ifthenelse of exp * exp * exp |
 	Let of ide * exp * exp | Fun of ide * exp | FunCall of exp * exp | Letrec of ide * exp * exp | 
 	ETree of tree | 
-	ApplyOver of (ide list) exp * exp |
-	Select of ide * exp;;
-
+	ApplyOver of (ide list) * exp * exp |
+	Select of ide * exp 
+	and tree = Empty | Node of ide * exp * tree * tree;;
+	
 (*ambiente polimorfo*)
 type 't env = ide -> 't;;
 let emptyenv (v : 't) = function x -> v;;
 let applyenv (r : 't env) (i : ide) = r i;;
 let bind (r : 't env) (i : ide) (v : 't) = function x -> if x = i then v else applyenv r x;;
 
-(*tipi esprimibili*)
-type evT = Int of int | Bool of bool | Unbound | FunVal of evFun | RecFunVal of ide * evFun
+(*tipi esprimibili
+Gli alberi sono considerati come tipi esprimibili, in quanto la valutazione di Select restituisce un albero
+*)
+type evT = Int of int | Bool of bool | Tree of evTree | Unbound | FunVal of evFun | RecFunVal of ide * evFun
 and evFun = ide * exp * evT env
+and evTree = Empty | Node of ide * evT * evT * evT;;
 
 (*rts*)
 (*type checking*)
@@ -25,59 +29,75 @@ let typecheck (s : string) (v : evT) : bool = match s with
 	"bool" -> (match v with
 		Bool(_) -> true |
 		_ -> false) |
+	"tree" -> (match v with
+		Tree(_) -> true |
+		_ -> false) |		
 	_ -> failwith("not a valid type");;
 
 (*funzioni primitive*)
 let prod x y = if (typecheck "int" x) && (typecheck "int" y)
 	then (match (x,y) with
-		(Int(n),Int(u)) -> Int(n*u))
+		(Int(n),Int(u)) -> Int(n*u) |
+		_ -> failwith("Type error - typecheck failed"))
 	else failwith("Type error");;
 
 let sum x y = if (typecheck "int" x) && (typecheck "int" y)
 	then (match (x,y) with
-		(Int(n),Int(u)) -> Int(n+u))
+		(Int(n),Int(u)) -> Int(n+u) |
+		_ -> failwith("Type error - typecheck failed"))
 	else failwith("Type error");;
 
 let diff x y = if (typecheck "int" x) && (typecheck "int" y)
 	then (match (x,y) with
-		(Int(n),Int(u)) -> Int(n-u))
+		(Int(n),Int(u)) -> Int(n-u) |
+		_ -> failwith("Type error - typecheck failed"))
 	else failwith("Type error");;
 
 let eq x y = if (typecheck "int" x) && (typecheck "int" y)
 	then (match (x,y) with
-		(Int(n),Int(u)) -> Bool(n=u))
+		(Int(n),Int(u)) -> Bool(n=u) |
+		_ -> failwith("Type error - typecheck failed"))
 	else failwith("Type error");;
 
 let minus x = if (typecheck "int" x) 
 	then (match x with
-	   	Int(n) -> Int(-n))
+	   	Int(n) -> Int(-n) |
+		_ -> failwith("Type error - typecheck failed"))
 	else failwith("Type error");;
 
 let iszero x = if (typecheck "int" x)
 	then (match x with
-		Int(n) -> Bool(n=0))
+		Int(n) -> Bool(n=0) |
+		_ -> failwith("Type error - typecheck failed"))
 	else failwith("Type error");;
 
 let vel x y = if (typecheck "bool" x) && (typecheck "bool" y)
 	then (match (x,y) with
-		(Bool(b),Bool(e)) -> (Bool(b||e)))
+		(Bool(b),Bool(e)) -> (Bool(b||e)) |
+		_ -> failwith("Type error - typecheck failed"))
 	else failwith("Type error");;
 
 let et x y = if (typecheck "bool" x) && (typecheck "bool" y)
 	then (match (x,y) with
-		(Bool(b),Bool(e)) -> Bool(b&&e))
+		(Bool(b),Bool(e)) -> Bool(b&&e) |
+		_ -> failwith("Type error - typecheck failed"))
 	else failwith("Type error");;
 
 let non x = if (typecheck "bool" x)
 	then (match x with
 		Bool(true) -> Bool(false) |
-		Bool(false) -> Bool(true))
+		Bool(false) -> Bool(true) |
+		_ -> failwith("Type error - typecheck failed"))
 	else failwith("Type error");;
 
 (*interprete*)
 let rec eval (e : exp) (r : evT env) : evT = match e with
 	Eint n -> Int n |
 	Ebool b -> Bool b |
+	ETree(Empty) -> Tree(Empty) |
+	ETree(Node(i,en,t1,t2)) ->	let ris1 = eval (ETree(t1)) r in
+							   	let ris2 = eval (ETree(t2)) r in	
+								Tree(Node(i,eval en r,ris1, ris2)) |
 	IsZero a -> iszero (eval a r) |
 	Den i -> applyenv r i |
 	Eq(a, b) -> eq (eval a r) (eval b r) |
@@ -96,7 +116,7 @@ let rec eval (e : exp) (r : evT env) : evT = match e with
 	Let(i, e1, e2) -> eval e2 (bind r i (eval e1 r)) |
 	Fun(i, a) -> FunVal(i, a, r) |
 	FunCall(f, eArg) -> 
-		let fClosure = (eval f r) in
+		(let fClosure = (eval f r) in
 			(match fClosure with
 				FunVal(arg, fBody, fDecEnv) -> 
 					eval fBody (bind fDecEnv arg (eval eArg r)) |
@@ -105,13 +125,23 @@ let rec eval (e : exp) (r : evT env) : evT = match e with
 						let rEnv = (bind fDecEnv g fClosure) in
 							let aEnv = (bind rEnv arg aVal) in
 								eval fBody aEnv |
-				_ -> failwith("non functional value")) |
-        Letrec(f, funDef, letBody) ->
-        		(match funDef with
-            		Fun(i, fBody) -> let r1 = (bind r f (RecFunVal(f, (i, fBody, r)))) in
-                         			                eval letBody r1 |
-            		_ -> failwith("non functional def"));;
-		
+				_ -> failwith("non functional value"))) |
+	Letrec(f, funDef, letBody) ->
+			(match funDef with
+				Fun(i, fBody) -> (let r1 = (bind r f (RecFunVal(f, (i, fBody, r)))) in
+												eval letBody r1) |
+				_ -> failwith("non functional def")) |
+	Select(i,et) ->  (match et with
+						ETree(Empty) -> Tree(Empty) |
+						ETree(Node(idn,en,t1,t2)) -> (if i = idn then eval et r
+													 else let s1 = eval (Select(i,ETree(t1))) r in
+														 (match s1 with
+														 	Tree(Empty) -> eval (Select(i,ETree(t2))) r |
+															_ -> s1))  |
+						_ -> failwith("non tree def")) |
+	ApplyOver(tags,f,eArg) ->  failwith("NON ANCORA IMPLEMENTATO");;
+
+
 (* =============================  TESTS  ================= *)
 
 (* basico: no let *)
@@ -124,3 +154,63 @@ eval e1 env0;;
 let e2 = FunCall(Let("x", Eint 2, Fun("y", Sum(Den "y", Den "x"))), Eint 3);;
 
 eval e2 env0;;
+
+(* =============================  TESTS per alberi di espressioni  ================= *)
+print_endline "***1 Test alberi di espressioni***";;
+(* 1 Test Select(i,et) *)
+
+let t1 = ETree(
+			Node("a",Sum(Eint 2, Eint 2), 
+				Node("b",Prod(Eint 1,Eint 3),  
+					Node("d",Fun("y", Sum(Den "y", Eint 1)),
+						Empty,
+						Empty
+						),
+					Node("d",Eint(5),
+						Empty,
+						Empty
+						)
+					),
+				Node("b",Minus(Eint 2),
+					Empty,
+					Node("a", 
+						Eint(6),
+						Empty,
+						Empty
+						)
+					)
+				)
+			);;
+(* 1.1 selezione nodo "a" presente nella radice e in un altro nodo dell'albero. *)
+print_endline "1.1";;
+assert (match eval (Select("a", t1)) env0 with 
+		Tree
+		(Node ("a", Int 3,
+			Tree
+			(Node ("b", Int 3,
+				Tree
+				(Node ("d", FunVal ("y", Sum (Den "y", Eint 1), ev0), Tree Empty,
+				Tree Empty)),
+				Tree (Node ("d", Int 5, Tree Empty, Tree Empty)))),
+			Tree
+			(Node ("b", Int (-2), Tree Empty,
+				Tree (Node ("a", Int 6, Tree Empty, Tree Empty)))))) -> true |
+		_ -> false
+		);;
+(* 1.2 selezione nodo "b" presente anche nell'altro nodo allo stesso livello. *)
+print_endline "1.2";;
+assert (match eval (Select("b", t1)) env0 with 
+		Tree
+		(Node ("b", Int 3,
+		Tree
+			(Node ("d", FunVal ("y", Sum (Den "y", Eint 1), ev0), Tree Empty,
+			Tree Empty)),
+		Tree (Node ("d", Int 5, Tree Empty, Tree Empty)))) -> true |
+		_ -> false
+		);;
+(* 1.3 selezione nodo non presente *)
+print_endline "1.3";;
+assert (match eval (Select("c", t1)) env0 with 
+		Tree(Empty) -> true |
+		_ -> false
+		);;
